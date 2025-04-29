f9ee4eb6f2fad9c194761fc5b3e1eb7b8c44557f
olinwiol <dotsomewhat@gmail.com>
2025-04-29 16:54:47 +0200

    redo hyprpaper

--- hypr/scripts/wallpaper-picker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPTS=$(find ~/wallpapers -name "*.sh" | sort)
-SELECTED=$(echo "$SCRIPTS" | wofi --dmenu --prompt "Välj bakgrund")
-
-if [ -n "$SELECTED" ]; then
-    bash "$SELECTED"
-fi
