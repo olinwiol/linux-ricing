930903e0e7faf7dd197d738f12ec0c8d3f250e6f
olinwiol <dotsomewhat@gmail.com>
2025-04-29 13:57:14 +0200

    fix: agent-setup

--- ssh/agent-setup.sh
+++ ssh/agent-setup.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# Check if an agent is already running
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-	eval "$(ssh-agent -s)" > /dev/null 2>&1
+    eval "$(ssh-agent -s)" > /dev/null 2>&1
 fi
 
-ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+# Add key only if not already added
+ssh-add -l > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+fi
