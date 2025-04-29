73c1405c1291687738f17a9853d16687084eb608
olinwiol <dotsomewhat@gmail.com>
2025-04-29 16:58:50 +0200

    wallpaper logic

--- hypr/scripts/wallpaper_picker.sh
+++ hypr/scripts/wallpaper_picker.sh
@@ -1,1 +1,7 @@
+# Select a wallpaper using wofi
+SELECTED=$(find ~/Pictures/wallpapers -type f -name "*.jpg" -o -name "*.png" | wofi --dmenu --prompt "Välj bakgrund")
 
+# If a wallpaper is selected, set it
+if [ -n "$SELECTED" ]; then
+    hyprctl hyprpaper wallpaper "eDP-1,$SELECTED"
+fi
